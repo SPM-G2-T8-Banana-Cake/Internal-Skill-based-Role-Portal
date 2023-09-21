17c06f073b74ca49269d026a94ad0f1364212bcf
Janeleq <janelim2001@gmail.com>
2023-09-21 18:00:07 +0800

    Refine HR Home and Create Role Routes
    Added flow of HR Routes

--- isbrp/src/App.js
+++ isbrp/src/App.js
@@ -2,7 +2,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.css';
 import './styles/App.scss';
 import Login from "./pages/Login";
-import CreateRoleListing from "./pages/HR/CreateRoleListing";
+import HrHome from "./pages/HR/Home";
+import RolesManagement from "./pages/HR/RolesManagement/RolesManagement";
+import CreateRoleListing from "./pages/HR/RolesManagement/CreateRoleListing";
+
 
 function App() {
   return (
@@ -10,6 +13,8 @@ function App() {
       <Router>
         <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/home" element={<HrHome />} />
+        <Route path="/roles-management" element={<RolesManagement />} />
         <Route path="/create-role-listing" element={<CreateRoleListing />} />
         </Routes>
       </Router>
