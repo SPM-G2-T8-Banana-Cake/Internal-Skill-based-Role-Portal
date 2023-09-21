91acb7a4f76a35c0f3f8d8c2bc93936517188acb
Janeleq <janelim2001@gmail.com>
2023-09-21 18:02:41 +0800

    Update Page Layouts
    Formatted styling of pages

---
+++ isbrp/src/pages/HR/Home.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react';
+import Container from 'react-bootstrap/esm/Container.js';
+import HrHeader from "../../components/Header/HrHeader";
+import Footer from "../../components/Footer/Footer";
+
+function HrHome () {
+    useEffect(() => {
+        document.title = 'Home'
+    }, [])
+
+    return (
+        <>
+            <HrHeader />
+
+            <Container fluid className='contentBox p-4'>
+                <h1>Notifications</h1>
+                <hr />
+                <div className='bg-grey rounded p-3'>
+                    <h4>Roles was updated.</h4>
+                    <p>Something was updated by someone at dd/mm/yy, hh:mm am.</p>
+                </div>
+            </Container>
+            <Footer type={'bg-secondary'}  />
+        </>
+    );
+}
+
+export default HrHome;
