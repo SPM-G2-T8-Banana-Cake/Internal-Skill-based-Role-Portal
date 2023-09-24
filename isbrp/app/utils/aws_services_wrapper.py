67b3781c684e08ba634b64889b4d355fe5d43faa
peterwengg <peterwengjy@gmail.com>
2023-09-25 00:23:52 +0800

    Updated aws services wrapper to initialise cursor
    Updated aws services wrapper to initialise cursor

--- isbrp/app/utils/aws_services_wrapper.py
+++ isbrp/app/utils/aws_services_wrapper.py
@@ -1,6 +1,4 @@
-
 import pymssql
-
 
 class SqlServicesWrapper:
 
@@ -10,37 +8,36 @@ class SqlServicesWrapper:
         self.password = ''
         self.db = 'is212g2t8db'
 
+        # Initialize the cursor when creating an instance
+        self.conn = self.connection()
+        self.cursor = self.conn.cursor()
+
     def connection(self):
-        return(pymssql.connect(self.host, self.username, self.password, self.db))
-    
-    ##require cursor to query
-    def cursor(self):
-        cursor = self.connection.cursor()
+        return pymssql.connect(self.host, self.username, self.password, self.db)
 
-app_sql_wrapper = SqlServicesWrapper()
-print(app_sql_wrapper)
-app_sql_wrapper.cursor.execute('SELECT * FROM spm.Staff')
-rows = app_sql_wrapper.cursor.fetchall()
+# print(app_sql_wrapper)
+# app_sql_wrapper.cursor.execute('SELECT * FROM spm.Staff')
+# rows = app_sql_wrapper.cursor.fetchall()
 
-for row in rows:
-    print(row)
+# for row in rows:
+#     print(row)
 
-# Sample SELECT query for the Role_Skill table
-app_sql_wrapper.cursor.execute('SELECT * FROM spm.Role_Skill')
-rows = app_sql_wrapper.cursor.fetchall()
+# # Sample SELECT query for the Role_Skill table
+# app_sql_wrapper.cursor.execute('SELECT * FROM spm.Role_Skill')
+# rows = app_sql_wrapper.cursor.fetchall()
 
-# Display the results
-for row in rows:
-    print(row)
+# # Display the results
+# for row in rows:
+#     print(row)
 
-# Sample SELECT query for the Staff_Skill table
-app_sql_wrapper.cursor.execute('SELECT * FROM spm.Staff_Skill')
-rows = app_sql_wrapper.cursor.fetchall()
+# # Sample SELECT query for the Staff_Skill table
+# app_sql_wrapper.cursor.execute('SELECT * FROM spm.Staff_Skill')
+# rows = app_sql_wrapper.cursor.fetchall()
 
-# Display the results
-for row in rows:
-    print(row)
+# # Display the results
+# for row in rows:
+#     print(row)
 
-# Close cursor and connection
-app_sql_wrapper.cursor.close()
-app_sql_wrapper.conn.close()
+# # Close cursor and connection
+# app_sql_wrapper.cursor.close()
+# app_sql_wrapper.conn.close()
