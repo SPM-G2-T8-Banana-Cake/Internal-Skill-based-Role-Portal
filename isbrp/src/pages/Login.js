e09f84fef35e424477ed128d0137bb758e4eb6e6
Janeleq <janelim2001@gmail.com>
2023-10-02 21:39:52 +0800

    Update Login.js

--- isbrp/src/pages/Login.js
+++ isbrp/src/pages/Login.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import LoginHeader from "../components/Header/LoginHeader.js";
 import Footer from "../components/Footer/Footer";
 import Container from "react-bootstrap/Container";
