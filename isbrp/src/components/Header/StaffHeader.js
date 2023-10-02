ed84b34ee72f59e1842e98f290d465a8988a6489
Janeleq <janelim2001@gmail.com>
2023-10-02 21:40:04 +0800

    Create Staff header component

---
+++ isbrp/src/components/Header/StaffHeader.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { NavLink, useLocation } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar";
+import Image from "react-bootstrap/Image";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Nav from "react-bootstrap/Nav";
+import { FiLogOut, FiUser } from "react-icons/fi";
+import logo from "../../assets/logo.png";
+
+function StaffHeader() {
+  const location = useLocation();
+
+  const user = (
+    <span>
+      <FiUser />
+      <span className="mx-2">{location.state.id}</span>
+    </span>
+  );
+
+  return (
+    <Navbar fixed="top" expand="md" className="bg-secondary navbar-dark px-3">
+      <Navbar.Toggle className="text-dark" />
+      <Navbar.Brand>
+        <NavLink to="/staff-home" state={{ id: location.state.id }}>
+          <Image src={logo} alt="Logo" width="50" />
+        </NavLink>
+      </Navbar.Brand>
+      <Navbar.Collapse>
+        <Nav className="me-auto">
+          <NavLink
+            to=""
+            className="text-decoration-none text-dark me-3"
+            style={({ isActive, isPending }) => {
+              return {
+                fontWeight: isActive ? "bold" : "",
+                color: isPending ? "black" : "black",
+              };
+            }}
+            state={{ id: location.state.id }}
+          >
+            Avaliable Roles
+          </NavLink>
+        </Nav>
+        <NavDropdown title={user} className="text-dark">
+          <NavDropdown.Item href="/">
+            <FiLogOut className="me-2" />
+            Logout
+          </NavDropdown.Item>
+        </NavDropdown>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
+
+export default StaffHeader;
