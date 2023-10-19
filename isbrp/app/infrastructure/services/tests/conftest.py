875d8998a66a4678a0104eef06d926135e6ad012
peterwengg <peterwengjy@gmail.com>
2023-10-19 14:59:32 +0800

    Added unit test for create and view role posting
    Added unit test for create and view role posting

--- isbrp/app/infrastructure/services/tests/conftest.py
+++ isbrp/app/infrastructure/services/tests/conftest.py
@@ -1,5 +1,8 @@
 import pytest
+from flask import Flask, request, jsonify
+from server import app
 
 @pytest.fixture()
-def test():
-    return "test"
+def client():
+    with app.test_client() as client:
+        yield client
--- isbrp/app/infrastructure/services/tests/js_particulars_service_test.py
+++ isbrp/app/infrastructure/services/tests/js_particulars_service_test.py
@@ -1,5 +1,29 @@
-def test(test):
-    assert test == "test"
-    assert test != "other_than_test"
-    assert type(test) == str
-    assert str.lower(test) == test
+ROLE_LISTING_CREATE_JSON = {
+    "Role_Name" : "This Is Test Name", 
+    "Role_Desc" : "This Is Test Role Desc", 
+    "Dept" : "This Is Test Dept", 
+    "Application_Deadline" : "9999-12-31",
+    "Skill_Name" : "Test Skill"
+}
+ROLE_LISTING_CREATE_EXPECTED_STATUS = 200
+
+ROLE_LISTING_VIEW_EXPECTED_ARR_LEN = 0
+
+def test_default():
+    assert "test" == "test"
+
+def test_createRoleListing_validInputs_noSpecifiedListing(client):
+    response = client.post('/create_role_listing', json=ROLE_LISTING_CREATE_JSON)
+    response_status = response.status_code
+    print('\nTest:', 'test_createRoleListing_validInputs_noSpecifiedListing')
+    print('Expected:', ROLE_LISTING_CREATE_EXPECTED_STATUS)
+    print('Actual:', response_status)
+    assert ROLE_LISTING_CREATE_EXPECTED_STATUS == response_status
+
+def test_viewRoleListing_hasRoleListing_Pass(client):
+    response = client.get('/view_role_listings')
+    response_data_arr_len = len(response.get_json())
+    print('\nTest:', 'test_viewRoleListing_hasRoleListing_Pass')
+    print('Expected:', 'Arr Length >=', ROLE_LISTING_VIEW_EXPECTED_ARR_LEN) 
+    print('Actual:', 'Arr Length =', response_data_arr_len)
+    assert response_data_arr_len > ROLE_LISTING_VIEW_EXPECTED_ARR_LEN
