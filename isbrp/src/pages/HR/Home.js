85d342d7bc91349689d441326a7650198c06cb57
Janeleq <janelim2001@gmail.com>
2023-10-26 17:20:55 +0800

    Cleared console texts

--- isbrp/src/pages/HR/Home.js
+++ isbrp/src/pages/HR/Home.js
@@ -8,11 +8,10 @@ import { FiUser, FiBook } from "react-icons/fi";
 import bgIcon from "../../assets/staffHomeBg.png";
 import Row from "react-bootstrap/esm/Row";
 import Col from "react-bootstrap/esm/Col";
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
 import { FiMoreVertical } from "react-icons/fi";
-
 
 function HrHome() {
   const [roleListings, setRoleListings] = useState({});
@@ -21,37 +20,35 @@ function HrHome() {
   const handleChange = (acc) => (event, isExpanded) => {
     setExpanded(isExpanded ? acc : false);
   };
-  
+
   useEffect(() => {
     document.title = "Home";
     window.scrollTo(0, 0);
 
-      hrReadRoleListings()
-        .then(function (response) {
-          console.log("Read Role Listings Endpoint Called");
-          console.log(response);
-          if (response.data.length > 0) {
-            setNoRoleListings(response.data.length);
-            let data = {};
-            for (let i = 0; i < response.data.length; i++) {
-              if (!Object.keys(data).includes(response.data[i].Dept)) {
-                data[response.data[i].Dept] = [response.data[i].Role_Name];
-              } else {
-                data[response.data[i].Dept].push(response.data[i].Role_Name);
-              }
+    hrReadRoleListings()
+      .then(function (response) {
+        console.log("Read Role Listings Endpoint Called");
+        console.log(response);
+        if (response.data.length > 0) {
+          setNoRoleListings(response.data.length);
+          let data = {};
+          for (let i = 0; i < response.data.length; i++) {
+            if (!Object.keys(data).includes(response.data[i].Dept)) {
+              data[response.data[i].Dept] = [response.data[i].Role_Name];
+            } else {
+              data[response.data[i].Dept].push(response.data[i].Role_Name);
             }
-            setRoleListings(data);
           }
-        })
-        .catch(function (error) {
-          console.log(error);
-          setNoRoleListings(0);
-          setRoleListings({});
-        });
-    }
-  , []);
-  
-  console.log("Role Listing", roleListings);
+          setRoleListings(data);
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+        setNoRoleListings(0);
+        setRoleListings({});
+      });
+  }, []);
+
   return (
     <>
       <HrHeader />
@@ -61,7 +58,7 @@ function HrHome() {
         <hr />
         <Row>
           <Col xs={6} sm={5} md={4}>
-            <div className="bg-grey w-100 rounded p-3">
+            <div className="bg-background w-100 rounded p-3">
               <h4>
                 Role Listings&nbsp;
                 <Badge badgeContent={noRoleListings} color="primary">
@@ -70,33 +67,31 @@ function HrHome() {
               </h4>
               <p>Check out the role listings by {Object.keys(roleListings).length} departments below!</p>
 
-          
               {Object.entries(roleListings).map((roleNames, dept) => {
                 return (
-                    
-                        <Accordion className="bg-button mb-2" expanded={expanded === `acc${dept}`} onChange={handleChange(`acc${dept}`)}>
-        <AccordionSummary
-          expandIcon={<FiMoreVertical/>}
-        >
-       <p className="font-italic fw-bold">{Object.keys(roleListings)[dept]}        <Badge badgeContent={Object.values(roleListings)[dept].length} color="primary">
-                  <FiBook color="black" />
-                </Badge></p>
-        </AccordionSummary>
-        <AccordionDetails>
-        <ul>
-                      {roleListings[Object.keys(roleListings)[dept]].map((roleName, index) => {
-                        return <li key={roleName}>{roleName}</li>;
-                      })}
-                    </ul>
-        </AccordionDetails>
-      </Accordion>
-
+                  <Accordion className="border-1 bg-background mb-2" expanded={expanded === `acc${dept}`} onChange={handleChange(`acc${dept}`)}>
+                    <AccordionSummary expandIcon={<FiMoreVertical />}>
+                      <p className="font-italic fw-bold">
+                        {Object.keys(roleListings)[dept]}{" "}
+                        <Badge badgeContent={Object.values(roleListings)[dept].length} color="primary">
+                          <FiBook color="black" />
+                        </Badge>
+                      </p>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                      <ul>
+                        {roleListings[Object.keys(roleListings)[dept]].map((roleName, index) => {
+                          return <li key={roleName}>{roleName}</li>;
+                        })}
+                      </ul>
+                    </AccordionDetails>
+                  </Accordion>
                 );
               })}
             </div>
           </Col>
           <Col xs={4} md={4}>
-            <div className="bg-grey w-100 mb-3 rounded p-3">
+            <div className="bg-background w-100 mb-3 rounded p-3">
               <h4>
                 Applications &nbsp;
                 <Badge badgeContent={0} color="primary">
