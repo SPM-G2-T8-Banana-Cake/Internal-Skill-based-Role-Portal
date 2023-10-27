e06af68fab261ef5b575829b434290ed09bbc16a
peterwengg <peterwengjy@gmail.com>
2023-10-27 13:05:09 +0800

    Added requirements needed for scheduler
    Added requirements needed for scheduler

--- isbrp/app/server.py
+++ isbrp/app/server.py
@@ -24,7 +24,7 @@ def cron_update_staff_hrms(url = "http://localhost:5000/cron_update_staff_hrms")
 def cron_update_staff_skill_lms(url = "http://localhost:5000/cron_update_staff_skill_lms"):
     return requests.get(url, timeout=10000)
 
-# @scheduler.task('interval', id='cron_update_role_skill_ljps', seconds=4, misfire_grace_time=500)
+# @scheduler.task('interval', id='cron_update_role_skill_ljps', seconds=3.5, misfire_grace_time=500)
 @scheduler.task('cron', id='cron_update_role_skill_ljps', week='*', day_of_week='tue,wed,thu,fri,sat', hour=0, minute=3)
 def cron_update_role_skill_ljps(url = "http://localhost:5000/cron_update_role_skill_ljps"):
     return requests.get(url, timeout=10000)
